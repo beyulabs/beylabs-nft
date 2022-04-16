de2bf9ad61eef40ae87ed87e51ed18060458ef90
Ryan Harris <harrisryan1@gmail.com>
2022-04-16 17:10:25 -0400

    refactor contract

--- contracts/Nexus.sol
+++ contracts/Nexus.sol
@@ -1,6 +1,14 @@
 //SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0 <0.9.0;
 
+/* ======================
+ ▐ ▄ ▄▄▄ .▐▄• ▄ ▄• ▄▌.▄▄ ·
+•█▌▐█▀▄.▀· █▌█▌▪█▪██▌▐█ ▀.
+▐█▐▐▌▐▀▀▪▄ ·██· █▌▐█▌▄▀▀▀█▄
+██▐█▌▐█▄▄▌▪▐█·█▌▐█▄█▌▐█▄▪▐█
+▀▀ █▪ ▀▀▀ •▀▀ ▀▀ ▀▀▀  ▀▀▀▀
+====================== */
+
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -10,20 +18,20 @@ contract NexusProject is ERC721URIStorage, Ownable, ReentrancyGuard {
     using Counters for Counters.Counter;
     Counters.Counter private currentTokenId;
 
-    // Seating
+    // ---> Seating
     uint256 public constant MAX_CREW_SIZE = 10000;
 
-    // Boarding phases
+    // ---> Ticket prices
+    uint256 public constant PREBOARDING_PRICE = 0.03 ether;
+    uint256 public constant CREW_MINT_PRICE = 0.05 ether;
+    uint256 public constant FOUNDING_CAPTAIN_MINT_PRICE = 0.07 ether;
+    uint256 public constant CAPTAIN_MINT_PRICE = 0.08 ether;
+
+    // ---> Boarding phases
     bool public preboarding = false;
     bool public generalBoarding = false;
 
-    // Ticket prices
-    uint256 public constant PREBOARDING_PRICE = 0.03 ether;
-    uint256 public constant CREW_MINT_PRICE = 0.05 ether;
-    uint256 public constant CAPTAIN_MINT_PRICE = 0.08 ether;
-
     constructor() ERC721("Nexus Project", "NXS") {
-        // Start token ID count at 1 instead of 0
         currentTokenId.increment();
     }
 
@@ -46,42 +54,46 @@ contract NexusProject is ERC721URIStorage, Ownable, ReentrancyGuard {
         _;
     }
 
-    modifier paidForPreboarding(uint256 numToMint) {
-        require(
-                msg.value == numToMint * PREBOARDING_PRICE,
-                "Not enough ETH! Preboarding costs 0.03 ETH"
-        );
-        _;
+    modifier canEnlistEarly(uint256 numToMint, string jobTitle) {
+        if (jobTitle == 'captain') {
+            require(
+                    msg.value == numToMint * FOUNDING_CAPTAIN_MINT_PRICE,
+                    "Not enough ETH!"
+            );
+            _;
+        } else {
+            require(
+                    msg.value == numToMint * PREBOARDING_PRICE,
+                    "Not enough ETH!"
+            );
+            _;
+        }
     }
 
-    modifier paidForGeneralBoarding(uint256 numToMint) {
-        require(
-                msg.value == numToMint * CREW_MINT_PRICE,
-                "Not enough ETH! Boarding costs 0.05 ETH"
-        );
-        _;
-    }
-
-    modifier hasCaptainQualifications(uint256 numToMint) {
-        require(
-                preboarding || boarding,
-                "We aren't issuing captain's licenses at the moment. Sorry!"
-        );
-        require(
-                msg.value == numToMint * CAPTAIN_MINT_PRICE,
-                "Not enough ETH! A captain's license costs 0.08 ETH"
-        );
-        _;
+    modifier eligibleToEnlist(uint256 numToMint, string jobTitle) {
+        if (jobTitle == 'captain') {
+            require(
+                    msg.value == numToMint * CAPTAIN_MINT_PRICE,
+                    "Not enough ETH!"
+            );
+            _;
+        } else {
+            require(
+                    msg.value == numToMint * CREW_MINT_PRICE,
+                    "Not enough ETH!"
+            );
+            _;
+        }
     }
 
     // ---> Mint
-    function preMint(uint256 numToMint)
+    function preMint(uint256 numToMint, string jobTitle)
         public
         payable
         nonReentrant
         crewSpotsAvailable
         isPreboardingOpen
-        paidForPreboarding(numToMint)
+        canEnlistEarly(numToMint, jobTitle)
     {
         for (uint i = 0; i < numToMint; i++) {
             _safeMint(msg.sender, currentTokenId.current())
@@ -89,26 +101,13 @@ contract NexusProject is ERC721URIStorage, Ownable, ReentrancyGuard {
         }
     }
 
-    function crewMint(uint256 numToMint)
+    function mint(uint256 numToMint, string jobTitle)
         public
         payable
         nonReentrant
         crewSpotsAvailable
         isGeneralBoardingOpen
-        paidForGeneralBoarding(numToMint)
-    {
-        for (uint i = 0; i < numToMint; i++) {
-            _safeMint(msg.sender, currentTokenId.current())
-            currentTokenId.increment();
-        }
-    }
-
-    function capitainMint(uint256 numToMint)
-        public
-        payable
-        nonReentrant
-        crewSpotsAvailable
-        hasCaptainQualifications(numToMint)
+        eligibleToEnlist(numToMint, jobTitle)
     {
         for (uint i = 0; i < numToMint; i++) {
             _safeMint(msg.sender, currentTokenId.current())
