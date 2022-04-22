fbba940d352f6fff2e01df3b662c775c20ab290c
Ryan Harris <harrisryan1@gmail.com>
2022-04-22 19:30:08 -0400

    add first test

--- test/index.ts
+++ test/index.ts
@@ -15,12 +15,28 @@ describe("Nexus", function () {
 
     const preMintPrice = await nexus.FOUNDING_CREW_MINT_PRICE();
     const mintPrice = await nexus.CREW_MINT_PRICE();
-    const maxPerWallet = await nexus.MAX_CREW_SIZE();
+    const maxPerWallet = await nexus.MAX_TOKEN_PER_WALLET();
+
+    const foundingCrewSize = await nexus.MAX_FOUNDING_CREW_SIZE();
+    const crewSize = await nexus.MAX_CREW_SIZE();
+
     const baseURI = await nexus.BASE_URI();
 
     expect(preMintPrice).to.be.instanceOf(BigNumber);
+    expect(ethers.utils.formatEther(preMintPrice)).to.be.equal("0.07");
+
     expect(mintPrice).to.be.instanceOf(BigNumber);
+    expect(ethers.utils.formatEther(mintPrice)).to.be.equal("0.09");
+
     expect(maxPerWallet).to.be.instanceOf(BigNumber);
+    expect(ethers.utils.formatUnits(maxPerWallet, 0)).to.be.equal("5");
+
+    expect(foundingCrewSize).to.be.instanceOf(BigNumber);
+    expect(ethers.utils.formatUnits(foundingCrewSize, 0)).to.be.equal("1000");
+
+    expect(crewSize).to.be.instanceOf(BigNumber);
+    expect(ethers.utils.formatUnits(crewSize, 0)).to.be.equal("10000");
+
     expect(baseURI).to.be.equal("ipfs://xyz/");
   });
 });
