290f363e53c3056b7ed228797b8f8f3b2f2fc1b5
Ryan Harris <harrisryan1@gmail.com>
2022-04-24 13:43:49 -0400

    update test

--- test/index.ts
+++ test/index.ts
@@ -3,24 +3,29 @@ import { BigNumber } from "ethers";
 import { ethers } from "hardhat";
 
 describe("Nexus", function () {
-  it("properly deploys", async function () {
-    const Nexus = await ethers.getContractFactory("Nexus");
-    const nexus = await Nexus.deploy(
+  before(async function () {
+    this.Nexus = await ethers.getContractFactory("Nexus");
+  });
+
+  beforeEach(async function () {
+    this.nexus = await this.Nexus.deploy(
         1000,
         10000,
         5,
         "ipfs://xyz/"
     );
-    await nexus.deployed();
+    await this.nexus.deployed();
+  });
 
-    const preMintPrice = await nexus.FOUNDING_CREW_MINT_PRICE();
-    const mintPrice = await nexus.CREW_MINT_PRICE();
-    const maxPerWallet = await nexus.MAX_TOKEN_PER_WALLET();
+  it("properly deploys", async function () {
+    const preMintPrice = await this.nexus.FOUNDING_CREW_MINT_PRICE();
+    const mintPrice = await this.nexus.CREW_MINT_PRICE();
+    const maxPerWallet = await this.nexus.MAX_TOKEN_PER_WALLET();
 
-    const foundingCrewSize = await nexus.MAX_FOUNDING_CREW_SIZE();
-    const crewSize = await nexus.MAX_CREW_SIZE();
+    const foundingCrewSize = await this.nexus.MAX_FOUNDING_CREW_SIZE();
+    const crewSize = await this.nexus.MAX_CREW_SIZE();
 
-    const baseURI = await nexus.BASE_URI();
+    const baseURI = await this.nexus.BASE_URI();
 
     expect(preMintPrice).to.be.instanceOf(BigNumber);
     expect(ethers.utils.formatEther(preMintPrice)).to.be.equal("0.07");
@@ -41,62 +46,35 @@ describe("Nexus", function () {
   });
 
   it("toggles preboarding", async function () {
-    const Nexus = await ethers.getContractFactory("Nexus");
-    const nexus = await Nexus.deploy(
-        1000,
-        10000,
-        5,
-        "ipfs://xyz/"
-    );
-    await nexus.deployed();
-
-    let preboarding = await nexus.preboarding();
+    let preboarding = await this.nexus.preboarding();
     expect(preboarding).to.be.equal(false);
 
-    let togglePreboardingTxn = await nexus.togglePreboarding(true);
-    preboarding = await nexus.preboarding();
+    let togglePreboardingTxn = await this.nexus.togglePreboarding(true);
+    preboarding = await this.nexus.preboarding();
     expect(preboarding).to.be.equal(true);
 
-    togglePreboardingTxn = await nexus.togglePreboarding(false);
-    preboarding = await nexus.preboarding();
+    togglePreboardingTxn = await this.nexus.togglePreboarding(false);
+    preboarding = await this.nexus.preboarding();
     expect(preboarding).to.be.equal(false);
   });
 
   it("toggles boarding", async function () {
-    const Nexus = await ethers.getContractFactory("Nexus");
-    const nexus = await Nexus.deploy(
-        1000,
-        10000,
-        5,
-        "ipfs://xyz/"
-    );
-    await nexus.deployed();
-
-    let generalBoarding = await nexus.generalBoarding();
+    let generalBoarding = await this.nexus.generalBoarding();
     expect(generalBoarding).to.be.equal(false);
 
-    let toggleBoardingTxn = await nexus.togglePreboarding(true);
-    generalBoarding = await nexus.preboarding();
+    let toggleBoardingTxn = await this.nexus.togglePreboarding(true);
+    generalBoarding = await this.nexus.preboarding();
     expect(generalBoarding).to.be.equal(true);
 
-    toggleBoardingTxn = await nexus.togglePreboarding(false);
-    generalBoarding = await nexus.preboarding();
+    toggleBoardingTxn = await this.nexus.togglePreboarding(false);
+    generalBoarding = await this.nexus.preboarding();
     expect(generalBoarding).to.be.equal(false);
   });
 
   it("provides proper royalty info", async function () {
-    const Nexus = await ethers.getContractFactory("Nexus");
-    const nexus = await Nexus.deploy(
-        1000,
-        10000,
-        5,
-        "ipfs://xyz/"
-    );
-    await nexus.deployed();
+    const contractAddress = this.nexus.address;
 
-    const contractAddress = nexus.address;
-
-    let royaltyInfo = await nexus.royaltyInfo(
+    const royaltyInfo = await this.nexus.royaltyInfo(
         BigNumber.from("1"),
         BigNumber.from("1000000000000000000")
     );
