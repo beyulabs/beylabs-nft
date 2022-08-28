099f02b1a70aff2349e5c5bb38977d4a8489efb7
Ryan Harris <harrisryan1@gmail.com>
2022-08-28 15:03:01 -0400

    add ownership transfer to deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,10 +1,16 @@
 import { ethers } from "hardhat";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 async function main() {
   const NexusVoyagers = await ethers.getContractFactory("NexusVoyagers");
   const contract = await NexusVoyagers.deploy(100, 10, "ipfs://xyz");
 
   await contract.deployed();
+  await contract.transferOwnership(
+    process.env.NV_CONTRACT_OWNER_DEVELOPMENT as string
+  );
 
   console.log("NexusVoyagers deployed to:", contract.address);
 }
